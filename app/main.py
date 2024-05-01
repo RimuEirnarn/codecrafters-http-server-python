413dc3e8f375022575c6a557b55ee6dd7dbbadc0
RimuEirnarn <Rimuru720@Proton.me>
2024-05-01 21:51:20 +0700

    bind

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 # Uncomment this to pass the first stage
 import socket
 
-
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -9,8 +8,13 @@ def main():
     # Uncomment this to pass the first stage
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    client, addr = server_socket.accept() # wait for client
+    
+    print(f"Connected to {addr[0]}:{addr[1]}")
+    client.send(b"HTTP/1.1 200 OK\r\n\r\n")
 
+    client.close()
+    server_socket.close()
 
 if __name__ == "__main__":
     main()
